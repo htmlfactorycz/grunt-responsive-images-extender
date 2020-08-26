04679847bf12461bb0cf51d83287ccec70eb04ee
Viťa <petras@html-factory.cz>
2020-08-26 11:18:00 +0200

    Changed: plugin functions

--- tasks/responsive_images_extender.js
+++ tasks/responsive_images_extender.js
@@ -18,7 +18,6 @@ module.exports = function(grunt) {
   var path = require('path');
   var cheerio = require('cheerio');
   var sizeOf = require('image-size');
-  var Base64 = {_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/++[++^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
 
   var DEFAULT_OPTIONS = {
     separator: '-',
@@ -65,18 +64,14 @@ module.exports = function(grunt) {
       return srcMap;
     };
 
-    var buildSrcset = function(srcMap, imgElem) {
+    var buildSrcset = function(srcMap, imgSrc) {
       var srcset = [];
       var candidate;
 
       for (var img in srcMap) {
-        candidate = path.posix.join(path.dirname(imgElem.attr('src')), img);
+        candidate = path.posix.join(path.dirname(imgSrc), img);
         candidate += ' ' + srcMap[img] + 'w';
         srcset.push(candidate);
-      }
-
-      if (options.srcsetAttributeName !== DEFAULT_OPTIONS.srcsetAttributeName) {
-        imgElem.attr(DEFAULT_OPTIONS.srcsetAttributeName, null);
       }
 
       return srcset.join(', ');
@@ -98,51 +93,91 @@ module.exports = function(grunt) {
       return sizes.join(', ');
     };
 
-    var setSrcAttribute = function(imgElem, srcMap, filepath) {
-      switch (options.srcAttribute) {
-        case 'none':
-          return null;
-          break;
-        case 'smallest':
-          var smallestImage = Object.keys(srcMap).map(function(k) {
-            return [k, srcMap[k]];
-          }).reduce(function(a, b) {
-            return b[1] < a[1] ? b : a;
-          });
-          var src = path.posix.join(path.dirname(imgElem.attr('src')), smallestImage[0]);
+    var getSmallestImage = function(srcMap) {
+      var smallestImage = Object.keys(srcMap).map(function(k) {
+        return [k, srcMap[k]];
+      }).reduce(function(a, b) {
+        return b[1] < a[1] ? b : a;
+      });
 
-          //base64
-          if(smallestImage[1] <= 10){
-            var filePath = normalizeImagePath(src, filepath);
-            src = 'data:image/' + src.substr(src.lastIndexOf('.') + 1) + ';base64,' + Buffer.from(grunt.file.read(filePath.dir + '/' + filePath.base), { encoding: null }).toString('base64');
-          }
-
-          return src;
-          break;
-        default:
-      }
+      return smallestImage;
     };
 
-    var processImage = function(imgElem, filepath){
+    var processImage = function (imgElem, filepath, $) {
+      var picture = null;
       var imgSrc = imgElem.attr('src');
-      var imagePath = normalizeImagePath(imgSrc, filepath);
-      var imageMatches = findMatchingImages(imagePath);
 
-      switch (imageMatches.length) {
-        case 0:
-          grunt.verbose.error('Found no file for ' + imgSrc.cyan);
-          return;
-        case 1:
-          grunt.verbose.error('Found only one file for ' + imgSrc.cyan);
-          return;
-        default:
-          grunt.verbose.ok('Found ' + imageMatches.length.cyan + ' files for ' + imgSrc.cyan + ': ' + imageMatches);
+      var process = function(imgSrc, filepath){
+        var imagePath = normalizeImagePath(imgSrc, filepath);
+        var imageMatches = findMatchingImages(imagePath);
+
+        switch (imageMatches.length) {
+          case 0:
+            grunt.verbose.error('Found no file for ' + imgSrc.cyan);
+            return;
+          case 1:
+            grunt.verbose.error('Found only one file for ' + imgSrc.cyan);
+            return;
+          default:
+            grunt.verbose.ok('Found ' + imageMatches.length.cyan + ' files for ' + imgSrc.cyan + ': ' + imageMatches);
+        }
+
+        if (imageMatches.length) {
+          if (!picture) picture = '<picture>';
+
+          var srcMap = buildSrcMap(imageMatches, imagePath);
+          var smallestImage = getSmallestImage(srcMap);
+          var smallestImageSrc = path.posix.join(path.dirname(imgSrc), smallestImage[0]);
+
+          //base64 if smaller or equal to 10
+          if(smallestImage[1] <= 10){
+            smallestImageSrc = 'data:image/' + smallestImageSrc.substr(smallestImageSrc.lastIndexOf('.') + 1) + ';base64,' + Buffer.from(grunt.file.read(imagePath.dir + '/' + smallestImage[0], { encoding: null })).toString('base64');
+          }
+
+          picture += '<source srcset="' + smallestImageSrc + '" data-srcset="' + buildSrcset(srcMap, imgSrc) + '" type="image/' + imagePath.ext.split('.').pop() + '">';
+
+          if ('.webp' !== imagePath.ext) {
+            imgElem.attr('data-srcset', buildSrcset(srcMap, imgSrc));
+            imgElem.attr('src', smallestImageSrc);
+          }
+        }
       }
 
-      var srcMap = buildSrcMap(imageMatches, imagePath);
+      if(options.webp){
+        var splittedSrc = imgSrc.split(".");
+        splittedSrc[splittedSrc.length - 1] = 'webp';
+        var webpImgSrc = splittedSrc.join('.');
 
-      imgElem.attr(options.srcsetAttributeName, buildSrcset(srcMap, imgElem));
-      imgElem.attr('src', setSrcAttribute(imgElem, srcMap, filepath));
+        process(webpImgSrc, filepath);
+      }
+
+      process(imgSrc, filepath);
+
+      if (picture) {
+        picture += '</picture>';
+        imgElem.after(picture);
+
+        var lazyPicture = imgElem.next('picture');
+        lazyPicture.append(imgElem);
+
+        lazyPicture.after('<noscript></noscript>');
+        var noscript = lazyPicture.next('noscript');
+
+        noscript.append(lazyPicture.clone());
+        lazyPicture.addClass('lazyload');
+
+        noscript.find('source').each(function () {
+          $(this).attr('srcset', $(this).attr('data-srcset'));
+          $(this).removeAttr('data-srcset');
+        });
+
+        noscript.find('img').each(function () {
+          $(this).attr('srcset', $(this).attr('data-srcset'));
+          $(this).removeAttr('data-srcset');
+        });
+      }
+
+
     }
 
     var parseAndExtendImg = function(filepath) {
@@ -150,13 +185,11 @@ module.exports = function(grunt) {
       var $ = cheerio.load(content, {decodeEntities: false});
       var imgElems = $('img:not(' + options.ignore.join(', ') + ')');
 
-      //grunt.log.write(imgElems);
-
       imgElems.each(function() {
         var imgElem = $(this);
-        var hasSrcset = imgElem.attr(options.srcsetAttributeName) !== undefined;
+        var hasSrcset = imgElem.attr('data-srcset') !== undefined;
 
-        if (!hasSrcset) processImage(imgElem, filepath);
+        if (!hasSrcset) processImage(imgElem, filepath, $);
       });
 
       return {content: $.html(), count: imgElems.length};
